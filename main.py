17e261019f8480654f72de213ae747878243cbb4
dalio123 <37671667+dalio123@users.noreply.github.com>
2021-08-15 12:00:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #dipendenze non toccare
 import discord
+from discord.ext import commands
 import random
 from voti import voto
+from lunghezza import lung
+from discord.ext.commands import Bot
+
+client = discord.ext.commands.Bot(command_prefix = "!");
+#variabili
+
+#client = Bot("!")
+
 
 #token da cambiare se non va
 TOKEN = 'token'
 
-client = discord.Client()
+#client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -21,10 +30,29 @@ async def on_message(message):
 
 	if message.author == client.user:
 		return
+#stop
 
 	if user_message.lower() == '!r':
 		risposta = random.choice(voto)
+		
 		await message.channel.send(risposta)
+
+	elif user_message.lower() == '!l':
+			lunghezz = random.choice(lung)
+			await message.channel.send(lunghezz)
+
+
+
 	return
+
+
+
+
+#	if user_message.lower() == '!r':
+#		risposta = f'Bel cazzo, molto succoso, è proprio rosso come me, si merita un bel {random.randrange(11)}'
+#		await message.channel.send(risposta)
+#	return
+
+
 
 client.run(TOKEN)
